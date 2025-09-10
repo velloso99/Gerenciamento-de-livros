3fe89118d302f8a295676ebea89ca1083c97d3c8
Rodrigo Lopes Velloso <135156904+velloso99@users.noreply.github.com>
2025-09-10 15:52:21 -0300

    colors

---
+++ colors.py
@@ -0,0 +1,14 @@
+# CORES
+
+co0 = "#2e2d2b"  #Preta
+co1 = "#feffff"  #Branca
+co2 = "#4fa882"  #verde
+co3 = "#38576b"  #Valor
+co4 = "#403d3d"  #Letra
+co5 = "#e06636"  #-Profit
+co6 = "#E9A178"  
+co7 = "#3fbfb9"  #Verde
+co8 = "#263238"  # +Verde
+co9 = "#39edf5"  # +verde
+co10 = "#6e8faf"  
+co11 = "#f2f4f2" 
---
+++ tela.py
@@ -0,0 +1,42 @@
+from tkinter .ttk import*
+from  tkinter import *
+
+from colors import *
+
+
+###########################################################################################
+# Crando Janela
+janela = Tk()
+janela.title("Gerenciamento de Livros")
+janela.geometry('770x330')
+janela.configure(background=co1)
+janela.resizable(width=False, height=False)
+#root.overrideredirect(1)
+largura_root = 770
+altura_root = 330
+#obter tamanho da tela
+largura_tela = janela.winfo_screenwidth()
+altura_tela = janela.winfo_screenheight()
+# Calcular posição para centralizar
+pos_x = ( largura_tela-largura_root )//2
+pos_y = (altura_tela - altura_root)//2
+# Definir geometria da janela (LxA+X+Y)
+janela.geometry(f"{largura_root}x{altura_root}+{pos_x}+{pos_y}")
+
+Style = Style(janela)
+Style.theme_use("clam")
+
+#############################################################################################
+# Frames
+
+frame_cima= Frame(janela, width=770, height=50, bg=co6, relief="flat")
+frame_cima.grid(row=0 , column=0, columnspan=2, sticky=NSEW)
+
+
+
+
+
+janela.mainloop()
+
+
+
